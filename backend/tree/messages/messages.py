428ebb1a95e8a6a7b90c65c370b930ebfe85b73c
Mariya RAKHMAN <mrakhman@student.42.fr>
2019-07-27 20:30:49 +0200

    Merge branch 'master' of /Users/mrakhman/rakhmasha/matcha with conflicts.

--- backend/tree/messages/messages.py
+++ backend/tree/messages/messages.py
@@ -1,10 +1,25 @@
-from flask import blueprints, jsonify, request, g
+import http
 
+from flask import blueprints, jsonify, abort, request, g
+
+from models.like import Like
 from models.message import Message
+from models.user import User
 from utils.form_validator import check_fields
 from utils.security import authorised_only
 
 messages = blueprints.Blueprint("messages", __name__)
+
+
+@messages.route("/allowed/<int:user_id>", methods=['GET'])
+@authorised_only
+def users_can_chat(user_id):
+	sender_id = g.current_user.id
+	receiver_id = user_id
+	if (Like.is_liked(sender_id, receiver_id) and Like.is_liked(receiver_id, sender_id)) \
+		and not (User.user_is_blocked(sender_id, receiver_id) or User.user_is_blocked(receiver_id, sender_id)):
+		return jsonify(ok=True)
+	return jsonify(ok=False)
 
 
 @messages.route("/new", methods=['POST'])
@@ -28,9 +43,13 @@ def create_message():
 	}
 	check_fields(req_data, form_values)
 	sender_id = g.current_user.id
-	message = Message.from_dict({"sender_id": sender_id, "receiver_id": int(req_data["receiver_id"]), "text": req_data["text"]})
-	message.create()
-	return jsonify(ok=True)
+	receiver_id = int(req_data["receiver_id"])
+	if (Like.is_liked(sender_id, receiver_id) and Like.is_liked(receiver_id, sender_id)) \
+			and (not User.user_is_blocked(sender_id, receiver_id) and not User.user_is_blocked(receiver_id, sender_id)):
+		message = Message.from_dict({"sender_id": sender_id, "receiver_id": receiver_id, "text": req_data["text"]})
+		message.create()
+		return jsonify(ok=True)
+	abort(http.HTTPStatus.UNAUTHORIZED)
 
 
 @messages.route("/chats", methods=['GET'])
@@ -46,4 +65,3 @@ def get_chat_messages(user_id):
 	chat_messages = Message.get_chat_messages(g.current_user.id, user_id)
 	msgs = list(map(lambda msg: msg.get_view('chat'), chat_messages))
 	return jsonify(messages=msgs)
-
