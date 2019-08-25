ae366bf6afa4ee1315875359e8fe8f8b97b817f0
Mariya RAKHMAN <mrakhman@student.42.fr>
2019-08-25 14:56:48 +0200

    front: home page edited

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -8,7 +8,7 @@
         <p>Remember: you are invisible for other users until you have a  <router-link v-bind:to="'/my_profile'">profile image</router-link> and main profile info</p>
         <p>In <router-link v-bind:to="'/search'">search users list</router-link> visit other users' profile pages to like, block, report them as fake or chat with them</p>
         <p>When another user likes you back, you will see "Go to chat!" button on their <router-link v-bind:to="'/users/48'">profile page</router-link> (just an example)</p>
-        <p>All your <router-link v-bind:to="'/chat'">chats are here</router-link>. You can chat only with mutually liked users. If one of you unliked another, chat history will be still accessible</p>
+        <p>All your <router-link v-bind:to="'/chat'">chats are here</router-link>. You can only chat with mutually liked and not blocked users. If one of you unliked or blocked another, chat history won't be lost</p>
         <p>You will receive real-time <router-link v-bind:to="'/notifications'">notifications</router-link> about likes, unlikes, new messages and profile views</p>
         <p>We save <router-link v-bind:to="'/history'">history</router-link> of profiles that you visited. Click the profile link to visit user's page</p>
     </div>
